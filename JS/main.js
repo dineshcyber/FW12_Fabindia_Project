b3af610ce513a2b6becbdb6a3ca4ece7d2fa4161
Sayak Mohanta <sayakmohanta1998@gmail.com>
2021-09-29 12:11:15 +0530

    Basic changes

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,10 @@
-
-window.onscroll = function() {myFunction()};
-
+window.onscroll = function () {
+  myFunction();
+};
 
 var header = document.getElementById("myHeader");
 
 var sticky = header.offsetTop;
-
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
@@ -15,34 +14,33 @@ function myFunction() {
   }
 }
 
-
 let images = [
-    "https://www.fabindia.com/file/general/hp-mb-14sep21-1.jpg",
-    "https://www.fabindia.com/file/general/hp-mb-27sep21-4.jpg",
-    "https://www.fabindia.com/file/general/hp-mb-27sep21-5.jpg",
-    "https://www.fabindia.com/file/general/hp-mb-27sep21-2.jpg"
-  ];
+  "https://www.fabindia.com/file/general/hp-mb-14sep21-1.jpg",
+  "https://www.fabindia.com/file/general/hp-mb-27sep21-4.jpg",
+  "https://www.fabindia.com/file/general/hp-mb-27sep21-5.jpg",
+  "https://www.fabindia.com/file/general/hp-mb-27sep21-2.jpg",
+];
 
+let container = document.getElementById("mySlideshow");
+let interval;
 
-    let container=document.getElementById("mySlideshow");
-    let interval;
-
-    function startSlideshow(){
-        //1.
-        let counter=0;
-        interval = setInterval( function() {
-            container.innerHTML = null;
-            if(counter === images.length){
-                counter=0;
-            }
-            let img = document.createElement("img");
-        img.src=images[counter];
-        container.append(img);
-        counter = counter+1;
-    }, 3000);
+function startSlideshow() {
+  //1.
+  let counter = 0;
+  interval = setInterval(function () {
+    container.innerHTML = null;
+    if (counter === images.length) {
+      counter = 0;
     }
-    startSlideshow();
-        // Get the modal
+    let img = document.createElement("img");
+    img.src = images[counter];
+    container.append(img);
+    counter = counter + 1;
+  }, 3000);
+}
+startSlideshow();
+
+// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -51,19 +49,19 @@ var btn = document.getElementById("signin");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+// When the user clicks the button, open the modal
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
