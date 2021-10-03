09d8849db621c66e49916ce58f639cceaec6e2d9
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2021-10-03 08:49:04 +0530

    Update men.js

--- men.js
+++ men.js
@@ -72,10 +72,9 @@ function showProducts() {
 
       product_name.textContent = product.name;
 
-      let wishlist = document.createElement("img");
+      let wishlist = document.createElement("div");
 
-      wishlist.src =
-        "file:///C:/Users/hgaut/Documents/GitHub/FW12_Fabindia_Project/image/icon-heart-black.png";
+      wishlist.src = "image/icon-heart-black.png";
 
       wishlist.setAttribute("id", "wishlist");
 
@@ -112,10 +111,6 @@ function sort1() {
       let product_name = document.createElement("p");
 
       product_name.textContent = product.name;
-<<<<<<< HEAD
-  
-    
-=======
 
       let wishlist = document.createElement("img");
 
@@ -123,113 +118,9 @@ function sort1() {
         "file:///C:/Users/hgaut/Documents/GitHub/FW12_Fabindia_Project/image/icon-heart-black.png";
 
       wishlist.setAttribute("id", "wishlist");
->>>>>>> main
 
       div.onclick = function () {
         visible(product);
-<<<<<<< HEAD
-      }
-      
-      div.append(img, product_name, product_price);
-  
-      parent.append(div);
-   
-     
-  
-  
- } });
-       
-   }
-  
-   showProducts();
-
-
-
-
-
-   //sorting
-
-   function sort1(){
-
-    parent.innerHTML = null; 
-    info.forEach(function (product){
-    
-     if(Number(product.cost <= 1500) && (product.category == "men")){
-       
-      
-          let div = document.createElement('div');
-          
-          let img = document.createElement('img');
-          
-          img.src = product.img;
-          
-          let product_price = document.createElement('p');  product_price.setAttribute('class','mrpred')
-         
-      
-          product_price.textContent = product.price;
-      
-          let product_name = document.createElement('p');
-      
-          product_name.textContent = product.name;
-      
-        
-    
-          div.onclick = function(){
-            visible(product);
-          }
-          
-          div.append(img, product_name, product_price);
-      
-          parent.append(div);
-          
-    }});  
-  
-  }
-  
-
-
-
-  //sorting2
-
-  function sort2(){
-  
-    parent.innerHTML = null; 
-    info.forEach(function (product){
-    
-     if(Number(product.cost > 1500 ) && (product.category == "men")){
-      
-      
-          let div = document.createElement('div');
-          
-          let img = document.createElement('img');
-          
-          img.src = product.img;
-          
-          let product_price = document.createElement('p');  product_price.setAttribute('class','mrpred')
-      
-          product_price.textContent = product.price;
-      
-          let product_name = document.createElement('p');
-      
-          product_name.textContent = product.name;
-      
-       
-
-          div.onclick = function(){
-            visible(product);
-          }
-    
-         div.append(img, product_name, product_price);
-      
-          parent.append(div);
-       
-       }});
-   
-     }
-
-     if(localStorage.getItem('visible') === null){
-      localStorage.setItem('visible',JSON.stringify([]));
-=======
       };
 
       div.append(wishlist, img, product_name, product_price);
@@ -274,7 +165,6 @@ function sort2() {
       div.append(wishlist, img, product_name, product_price);
 
       parent.append(div);
->>>>>>> main
     }
   });
 }
