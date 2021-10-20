e0fbe57d28a1f4135e6b4d35eaff9e2bbfd2ae23
Dinesh Kumar <kumardinesh.ari@gmail.com>
2021-10-20 18:35:13 +0530

    Update products.js

--- JS/products.js
+++ JS/products.js
@@ -19,16 +19,16 @@ function myFunction() {
 //mouseover start
 let links = document.getElementById(`scroller-b`).querySelectorAll(`p`);
 links[2].addEventListener(`click`, function () {
-  window.location.href = "women.html";
+  window.location.href = "./Pages/women.html";
 });
 links[3].addEventListener(`click`, function () {
-  window.location.href = "men.html";
+  window.location.href = "./Pages/men.html";
 });
 links[6].addEventListener(`click`, function () {
-  window.location.href = "decorative.html";
+  window.location.href = "./Pages/decorative.html";
 });
 links[1].addEventListener(`click`, function () {
-  window.location.href = "products.html";
+  window.location.href = "./Pages/products.html";
 });
 
 //input-bar
@@ -37,11 +37,11 @@ var warning = document.getElementById(`warning`);
 input.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     if (input.value == `men` || input.value == `MEN`) {
-      window.location.href = "men.html";
+      window.location.href = "./Pages/men.html";
     } else if (input.value == `women` || input.value == `WOMEN`) {
-      window.location.href = "women.html";
+      window.location.href = "./Pages/women.html";
     } else if (input.value == `decor` || input.value == `DECOR`) {
-      window.location.href = "decorative.html";
+      window.location.href = "./Pages/decorative.html";
     } else {
       warning.style.display = `block`;
       input.value = null;
@@ -299,7 +299,7 @@ function visible(pro) {
   take.push(pro);
   localStorage.setItem("visible", JSON.stringify(take));
 
-  window.location.href = "bigview.html";
+  window.location.href = "./Pages/bigview.html";
 }
 
 //-----Description and layout sort panel----
